76047ed8a24162ab5ac2e232dd2a48e70505f4c2
Al Nemec <al.nemec@gmail.com>
2013-12-05 11:01:40 -0600

    added vallencio js call

--- header.php
+++ header.php
@@ -108,6 +108,10 @@
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/mainactions.js"></script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/blurobjs.js"></script>
 
+
+<script src="<?php echo get_template_directory_uri(); ?>/library/scripts/vallenato.js" type="text/javascript"></script>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/styles/vallenato.css" type="text/css" media="screen">
+
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
