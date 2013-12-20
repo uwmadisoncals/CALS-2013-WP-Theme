fe2a7234b670c223a6832fbf368a08482f090cd7
Al Nemec <al.nemec@gmail.com>
2013-12-20 15:22:02 -0600

    Updated the 404 page

--- 404.php
+++ 404.php
@@ -18,7 +18,8 @@ get_header(); ?>
 
 			<article id="post-0" class="post error404 not-found">
 				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentyeleven' ); ?></h1>
+					<h1 class="entry-title">Something is amiss...</h1>
+					<!--<h2>This site has recently been redesigned and restructured to make content easier to find.  A brief search should do the trick.</h2>-->
 				</header>
 
 				<div class="entry-content">
@@ -26,22 +27,28 @@ get_header(); ?>
 
 					<?php get_search_form(); ?>
 
+<div class="widget">
+						<h2 class="widgettitle">Site Structure Guide</h2>
+						<ul>
+						<?php wp_nav_menu( array( 'theme_location' => 'footer1' ) ); ?>
+						<?php wp_nav_menu( array( 'theme_location' => 'footer2' ) ); ?>
+						</ul>
+					</div>
+						
 					<?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ), array( 'widget_id' => '404' ) ); ?>
 
-					<div class="widget">
+					<div class="widget" style="margin-right: 0px;">
 						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'twentyeleven' ); ?></h2>
 						<ul>
 						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
 						</ul>
 					</div>
+					
+					
+					
+					
 
-					<?php
-					/* translators: %1$s: smilie */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'twentyeleven' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', array('count' => 0 , 'dropdown' => 1 ), array( 'after_title' => '</h2>'.$archive_content ) );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+					<?php //the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
 				</div><!-- .entry-content -->
 			</article><!-- #post-0 -->
