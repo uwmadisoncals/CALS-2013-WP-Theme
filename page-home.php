0c5cb97d375d7dbab92b32a94bf1c1f94e71b09e
Al Nemec <al.nemec@gmail.com>
2014-03-19 11:18:36 -0500

    removed offset until new store mismatch

--- page-home.php
+++ page-home.php
@@ -167,7 +167,7 @@ get_header(); ?>
 							<h2>Announcements</h2>
 							
 							<?php switch_to_blog(19); ?>
-<?php query_posts("cat=17&posts_per_page=1&offset=1"); ?>
+<?php query_posts("cat=17&posts_per_page=1"); ?>
 <?php if (have_posts()) : ?>
   <?php while (have_posts()) : the_post();  ?>
 
