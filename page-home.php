e92e4657bc98d061abed2d8d17fa9c617d4df650
Al Nemec <al.nemec@gmail.com>
2014-05-09 08:49:34 -0500

    Updated events feed on the home page to use the uw madison plugin

--- page-home.php
+++ page-home.php
@@ -13,7 +13,7 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
- 
+
 //wp_enqueue_script( 'twentyeleven-showcase', get_template_directory_uri() . '/js/search.js', array( 'jquery' ), '2013-07-10' );
 
 get_header(); ?>
@@ -25,25 +25,25 @@ get_header(); ?>
 
 		<div id="primary">
 			<div id="content" class="fullWidth" role="main">
-				
-				
+
+
 				<!-- CALS News Content Box -->
 				<div class="row clearfix">
-				
+
 					<div class="span-50 box dropin">
-							
+
 							<h2>News</h2>
-							
+
 							<?php switch_to_blog(20); ?>
 <?php query_posts("posts_per_page=1&category_name=featured-articles"); ?>
 <?php if (have_posts()) : ?>
   <?php while (have_posts()) : the_post();  ?>
 
   <?php	if ( has_post_thumbnail() ) {
-		    				
+
 		    				//the_post_thumbnail();
 		    				echo get_the_post_thumbnail($page->ID, 'large');
- 
+
 		    				} else {
 							//echo "<img src='".get_template_directory_uri()."/images/newsplaceholder1.jpeg' alt=' '>";
 							 //echo '<img src="';
@@ -56,19 +56,19 @@ get_header(); ?>
 											<p><?php the_time('l, F jS, Y') ?></p>
                                              </div>
                             <div class="topShade"></div>
-							<div class="bottomShade"></div>			
-    
-    
+							<div class="bottomShade"></div>
 
-    
- 
+
+
+
+
 
   <?php endwhile; ?>
 <?php endif; ?>
-<?php restore_current_blog(); ?>							
+<?php restore_current_blog(); ?>
 							<a href="http://news.cals.wisc.edu" class="moreButton">More News</a>
-						
-						
+
+
 						<div class="windows8">
 							<div class="wBall" id="wBall_1">
 							<div class="wInnerBall">
@@ -90,35 +90,35 @@ get_header(); ?>
 							<div class="wInnerBall">
 							</div>
 							</div>
-						</div> 
+						</div>
 
 						<div class="shade"></div>
-						
+
 					</div>
-					
+
 					<div class="span-50 box dropin2">
-							 <?php 
+							 <?php
 								//hold original loop
 								$tmp_post = $post;
-								
+
 								//get spotlight posts
 								$posts = get_posts('category_name=spotlight&numberposts=1');
 								foreach($posts as $post){
 									setup_postdata($post);?>
-									
+
 								<?php	if ( has_post_thumbnail() ) {
-		    				
+
 		    				//the_post_thumbnail();
 		    				echo get_the_post_thumbnail($page->ID, 'large');
- 
+
 		    				} else {
- 
+
 							 //echo '<img src="';
 							 echo catch_that_image();
 							// echo '" alt="" />';
 
 						} ?>
-                                               
+
                                                 <h2>Spotlight</h2>
                                              <div class="boxContent">
 											<h3 class="spotlight_title"><a href="<?php the_permalink();?>"><?php  the_title();?></a></h3>
@@ -126,10 +126,10 @@ get_header(); ?>
                                              </div>
                                              <div class="topShade"></div>
 							<div class="bottomShade"></div>
-								<!-- .spotlight_slide -->  
-                        <?php	 
-							
-								} 
+								<!-- .spotlight_slide -->
+                        <?php
+
+								}
 								//restore original loop
 								$post = $tmp_post;
 							?>
@@ -154,30 +154,30 @@ get_header(); ?>
 							<div class="wInnerBall">
 							</div>
 							</div>
-						</div> 
+						</div>
 
-						<div class="shade"></div>	
+						<div class="shade"></div>
 					</div>
-					
+
 				</div>
-				
+
 				<div class="row clearfix">
-				
+
 					<div class="span-33 box dropin3">
 							<h2>Announcements</h2>
-							
+
 							<?php switch_to_blog(19); ?>
 <?php query_posts("cat=17&posts_per_page=1"); ?>
 <?php if (have_posts()) : ?>
   <?php while (have_posts()) : the_post();  ?>
 
   <?php	if ( has_post_thumbnail() ) {
-		    				
+
 		    				//the_post_thumbnail();
 		    				echo get_the_post_thumbnail($page->ID, 'large');
- 
+
 		    				} else {
-							
+
 							 //echo '<img src="';
 							 echo catch_that_announcements_image();
 							// echo '" alt="" />';
@@ -188,16 +188,16 @@ get_header(); ?>
 											<p><?php the_time('l, F jS, Y') ?></p>
                                              </div>
                             <div class="topShade"></div>
-							<div class="bottomShade"></div>			
-    
-    
+							<div class="bottomShade"></div>
 
-    
- 
+
+
+
+
 
   <?php endwhile; ?>
 <?php endif; ?>
-<?php restore_current_blog(); ?>							
+<?php restore_current_blog(); ?>
 							<a href="http://ecals.cals.wisc.edu" class="moreButton">More Announcements</a>
 						<div class="windows8">
 							<div class="wBall" id="wBall_1">
@@ -220,51 +220,28 @@ get_header(); ?>
 							<div class="wInnerBall">
 							</div>
 							</div>
-						</div> 
+						</div>
 
 						<div class="shade"></div>
 					</div>
-					
+
 					<div class="span-33 box eventsBox dropin4">
 							<h2>Events</h2>
 							<img src="<?php echo get_template_directory_uri(); ?>/images/aghall1.jpg" alt=" ">
 							<div class="boxContent">
-										<?php // Get RSS Feed(s)
-  include_once(ABSPATH . WPINC . '/rss.php');
-  $rss = fetch_rss('http://www.today.wisc.edu/events/feed/30.rss2');
-  $maxitems = 2;
-  $items = array_slice($rss->items, 0, $maxitems);
-?>
+	
+
+  <?php uwmadison_events('http://www.today.wisc.edu/events/feed/30', array('limit' => 2)) ?>
 
 
-  <?php if (empty($items)): ?>
-   <h3 class="spotlight_title">No Upcoming Events</h3>
-  <?php else:
-      foreach ( $items as $item ):
-        ?>
-        <h3 class="spotlight_title">
-          <a href='<?php echo $item['link']; ?>' title='<?php echo $item['title']; ?>'>
-            
-            <?php $tempTitle = $item['title']; $newTitle = substr($tempTitle, 20, 60); ?>
-            <?php echo $newTitle."..."; ?>
-          </a>
-        </h3>
-        <p><?php $tempDate = $item['title']; $newDate = substr($tempTitle, 0, 18); ?>
-            <?php echo $newDate; ?></p>
-        <?php
-      endforeach;
-    endif;
-  ?>
 
-				
-			
-								
+
                                              </div>
                             <div class="topShade"></div>
-							<div class="bottomShade"></div>			
-    
+							<div class="bottomShade"></div>
+
 								<a href="http://www.today.wisc.edu/events/feed/30" class="moreButton">More Events</a>
-								
+
 								<div class="windows8">
 							<div class="wBall" id="wBall_1">
 							<div class="wInnerBall">
@@ -286,25 +263,25 @@ get_header(); ?>
 							<div class="wInnerBall">
 							</div>
 							</div>
-						</div> 
+						</div>
 
 						<div class="shade"></div>
 
 					</div>
-					
+
 					<div class="span-33 box dropin5">
 							<h2>CALS Faces</h2>
-							
+
 							<?php switch_to_blog(20); ?>
 <?php query_posts("posts_per_page=1&cat=17"); ?>
 <?php if (have_posts()) : ?>
   <?php while (have_posts()) : the_post();  ?>
 
   <?php	if ( has_post_thumbnail() ) {
-		    				
+
 		    				//the_post_thumbnail();
 		    				echo get_the_post_thumbnail($page->ID, 'large');
- 
+
 		    				} else {
 							//echo "<img src='".get_template_directory_uri()."/images/newsplaceholder1.jpeg' alt=' '>";
 							 //echo '<img src="';
@@ -320,12 +297,12 @@ get_header(); ?>
 				} ?></p>
                                              </div>
                             <div class="topShade"></div>
-							<div class="bottomShade"></div>			
-    
-    
+							<div class="bottomShade"></div>
 
-    
- 
+
+
+
+
 
   <?php endwhile; ?>
 <?php endif; ?>
@@ -352,15 +329,15 @@ get_header(); ?>
 							<div class="wInnerBall">
 							</div>
 							</div>
-						</div> 
+						</div>
 
 						<div class="shade"></div>
 					</div>
-					
+
 				</div>
-				
+
 			</div><!-- #content -->
-			
+
 			<div class="clear"></div>
 		</div><!-- #primary -->
 
