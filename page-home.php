9e36eaa4d09eb77a24bd6f83fc7e1868f8ba75ea
Al Nemec <al.nemec@gmail.com>
2014-06-12 15:19:40 -0500

    Working on new home page promotion

--- page-home.php
+++ page-home.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * Template Name: Home Page
- *
- * Description: Twenty Twelve loves the no-sidebar look as much as
- * you do. Use this page template to remove the sidebar from any page.
- *
- * Tip: to remove the sidebar from all posts and pages simply remove
- * any active widgets from the Main Sidebar area, and the sidebar will
- * disappear everywhere.
- *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
- */
+* Template Name: Home Page
+*
+* Description: Twenty Twelve loves the no-sidebar look as much as
+* you do. Use this page template to remove the sidebar from any page.
+*
+* Tip: to remove the sidebar from all posts and pages simply remove
+* any active widgets from the Main Sidebar area, and the sidebar will
+* disappear everywhere.
+*
+* @package WordPress
+* @subpackage Twenty_Twelve
+* @since Twenty Twelve 1.0
+*/
 
 //wp_enqueue_script( 'twentyeleven-showcase', get_template_directory_uri() . '/js/search.js', array( 'jquery' ), '2013-07-10' );
 
@@ -27,6 +27,8 @@ get_header(); ?>
 			<div id="content" class="fullWidth" role="main">
 
 
+
+
 				<!-- CALS News Content Box -->
 				<div class="row clearfix">
 
@@ -37,25 +39,25 @@ get_header(); ?>
 							<?php switch_to_blog(20); ?>
 <?php query_posts("posts_per_page=1&category_name=featured-articles"); ?>
 <?php if (have_posts()) : ?>
-  <?php while (have_posts()) : the_post();  ?>
+	<?php while (have_posts()) : the_post();  ?>
 
-  <?php	if ( has_post_thumbnail() ) {
+	<?php	if ( has_post_thumbnail() ) {
 
-		    				//the_post_thumbnail();
-		    				echo get_the_post_thumbnail($page->ID, 'large');
+								//the_post_thumbnail();
+								echo get_the_post_thumbnail($page->ID, 'large');
 
-		    				} else {
+								} else {
 							//echo "<img src='".get_template_directory_uri()."/images/newsplaceholder1.jpeg' alt=' '>";
-							 //echo '<img src="';
-							 echo catch_that_news_image();
+							//echo '<img src="';
+							echo catch_that_news_image();
 							// echo '" alt="" />';
 
 						} ?>
 			<div class="boxContent">
 											<h3 class="spotlight_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a> </h3>
 											<p><?php the_time('l, F jS, Y') ?></p>
-                                             </div>
-                            <div class="topShade"></div>
+																						</div>
+														<div class="topShade"></div>
 							<div class="bottomShade"></div>
 
 
@@ -63,7 +65,7 @@ get_header(); ?>
 
 
 
-  <?php endwhile; ?>
+	<?php endwhile; ?>
 <?php endif; ?>
 <?php restore_current_blog(); ?>
 							<a href="http://news.cals.wisc.edu" class="moreButton">More News</a>
@@ -97,7 +99,7 @@ get_header(); ?>
 					</div>
 
 					<div class="span-50 box dropin2">
-							 <?php
+							<?php
 								//hold original loop
 								$tmp_post = $post;
 
@@ -108,26 +110,26 @@ get_header(); ?>
 
 								<?php	if ( has_post_thumbnail() ) {
 
-		    				//the_post_thumbnail();
-		    				echo get_the_post_thumbnail($page->ID, 'large');
+								//the_post_thumbnail();
+								echo get_the_post_thumbnail($page->ID, 'large');
 
-		    				} else {
+								} else {
 
-							 //echo '<img src="';
-							 echo catch_that_image();
+							//echo '<img src="';
+							echo catch_that_image();
 							// echo '" alt="" />';
 
 						} ?>
 
-                                                <h2>Spotlight</h2>
-                                             <div class="boxContent">
+																								<h2>Spotlight</h2>
+																						<div class="boxContent">
 											<h3 class="spotlight_title"><a href="<?php the_permalink();?>"><?php  the_title();?></a></h3>
 											<p><?php echo $post->post_excerpt;?></p>
-                                             </div>
-                                             <div class="topShade"></div>
+																						</div>
+																						<div class="topShade"></div>
 							<div class="bottomShade"></div>
 								<!-- .spotlight_slide -->
-                        <?php
+												<?php
 
 								}
 								//restore original loop
@@ -163,31 +165,82 @@ get_header(); ?>
 
 				<div class="row clearfix">
 
+					<div class="span-100 box dropin">
+
+						<div class="boxContent">
+														<h3 class="spotlight_title"><a href="#" rel="bookmark" title="Permanent Link to Title">Title</a> </h3>
+														<p>Date Stamp</p>
+																									</div>
+																	<div class="topShade"></div>
+										<div class="bottomShade"></div>
+
+
+
+
+
+
+
+
+
+									<div class="windows8">
+										<div class="wBall" id="wBall_1">
+										<div class="wInnerBall">
+										</div>
+										</div>
+										<div class="wBall" id="wBall_2">
+										<div class="wInnerBall">
+										</div>
+										</div>
+										<div class="wBall" id="wBall_3">
+										<div class="wInnerBall">
+										</div>
+										</div>
+										<div class="wBall" id="wBall_4">
+										<div class="wInnerBall">
+										</div>
+										</div>
+										<div class="wBall" id="wBall_5">
+										<div class="wInnerBall">
+										</div>
+										</div>
+									</div>
+
+									<div class="shade"></div>
+
+								</div>
+
+
+					</div>
+
+				</div>
+
+				<div class="row clearfix">
+
 					<div class="span-33 box dropin3">
 							<h2>Announcements</h2>
 
 							<?php switch_to_blog(19); ?>
 <?php query_posts("cat=17&posts_per_page=1"); ?>
 <?php if (have_posts()) : ?>
-  <?php while (have_posts()) : the_post();  ?>
+	<?php while (have_posts()) : the_post();  ?>
 
-  <?php	if ( has_post_thumbnail() ) {
+	<?php	if ( has_post_thumbnail() ) {
 
-		    				//the_post_thumbnail();
-		    				echo get_the_post_thumbnail($page->ID, 'large');
+								//the_post_thumbnail();
+								echo get_the_post_thumbnail($page->ID, 'large');
 
-		    				} else {
+								} else {
 
-							 //echo '<img src="';
-							 echo catch_that_announcements_image();
+							//echo '<img src="';
+							echo catch_that_announcements_image();
 							// echo '" alt="" />';
 
 						} ?>
 			<div class="boxContent">
 											<h3 class="spotlight_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a> </h3>
 											<p><?php the_time('l, F jS, Y') ?></p>
-                                             </div>
-                            <div class="topShade"></div>
+																						</div>
+														<div class="topShade"></div>
 							<div class="bottomShade"></div>
 
 
@@ -195,7 +248,7 @@ get_header(); ?>
 
 
 
-  <?php endwhile; ?>
+	<?php endwhile; ?>
 <?php endif; ?>
 <?php restore_current_blog(); ?>
 							<a href="http://ecals.cals.wisc.edu" class="moreButton">More Announcements</a>
@@ -229,15 +282,15 @@ get_header(); ?>
 							<h2>Events</h2>
 							<img src="<?php echo get_template_directory_uri(); ?>/images/aghall1.jpg" alt=" ">
 							<div class="boxContent">
-	
-
-  <?php uwmadison_events('http://www.today.wisc.edu/events/feed/30', array('limit' => 2)) ?>
 
 
+	<?php uwmadison_events('http://www.today.wisc.edu/events/feed/30', array('limit' => 2)) ?>
 
 
-                                             </div>
-                            <div class="topShade"></div>
+
+
+																						</div>
+														<div class="topShade"></div>
 							<div class="bottomShade"></div>
 
 								<a href="http://www.today.wisc.edu/events/feed/30" class="moreButton">More Events</a>
@@ -275,17 +328,17 @@ get_header(); ?>
 							<?php switch_to_blog(20); ?>
 <?php query_posts("posts_per_page=1&cat=17"); ?>
 <?php if (have_posts()) : ?>
-  <?php while (have_posts()) : the_post();  ?>
+	<?php while (have_posts()) : the_post();  ?>
 
-  <?php	if ( has_post_thumbnail() ) {
+	<?php	if ( has_post_thumbnail() ) {
 
-		    				//the_post_thumbnail();
-		    				echo get_the_post_thumbnail($page->ID, 'large');
+								//the_post_thumbnail();
+								echo get_the_post_thumbnail($page->ID, 'large');
 
-		    				} else {
+								} else {
 							//echo "<img src='".get_template_directory_uri()."/images/newsplaceholder1.jpeg' alt=' '>";
-							 //echo '<img src="';
-							 echo catch_that_news_image();
+							//echo '<img src="';
+							echo catch_that_news_image();
 							// echo '" alt="" />';
 
 						} ?>
@@ -295,8 +348,8 @@ get_header(); ?>
 				if($academic_info!=""){
 					echo $academic_info;
 				} ?></p>
-                                             </div>
-                            <div class="topShade"></div>
+																						</div>
+														<div class="topShade"></div>
 							<div class="bottomShade"></div>
 
 
@@ -304,7 +357,7 @@ get_header(); ?>
 
 
 
-  <?php endwhile; ?>
+	<?php endwhile; ?>
 <?php endif; ?>
 <?php restore_current_blog(); ?>
 <a href="http://news.cals.wisc.edu/category/departments/cals-faces/" class="moreButton">More CALS Faces</a>
