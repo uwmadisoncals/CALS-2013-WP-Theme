55a6cc2b947def7007142e4196563eb5e63f0014
Al Nemec <al.nemec@gmail.com>
2015-08-26 13:59:50 -0500

    Added new template for careers layout

--- content-page-features-careers.php
+++ content-page-features-careers.php
@@ -20,7 +20,7 @@
 
 	<div class="entry-content">
 		
-		<?php the_content(); ?>
+		
 		<div class="pagefeatureboxes">
 		<div class="row clearfix">
 						<div class="span-50 box fullbleed">
@@ -97,48 +97,14 @@
 					</div>
 					
 					<div class="row clearfix">
-						<div class="span-50 box workplace fullbleed">
+						<div class="span-50 box fullbleed">
 							<h2><?php the_field('feature_container_3_title'); ?></h2>
-							
-											
-											<?php switch_to_blog(19); ?>
-<?php query_posts("cat=17&posts_per_page=1"); ?>
-<?php if (have_posts()) : ?>
-  <?php while (have_posts()) : the_post();  ?>
-
-  <?php	if ( has_post_thumbnail() ) {
-		    				
-		    				//the_post_thumbnail();
-		    				echo get_the_post_thumbnail($page->ID, 'large');
- 
-		    				} else { ?>
 							<img src="<?php the_field('feature_container_3_image'); ?>" alt="" />
-							
-							<?php
-							 //echo '<img src="';
-							 //echo catch_that_announcements_image();
-							 
-							// echo '" alt="" />';
-
-						} ?>
-			<div class="boxContent">
-											<h3 class="spotlight_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a> </h3>
-											<p><?php the_time('l, F jS, Y') ?></p>
+							<div class="boxContent">
+											<?php the_field('feature_container_3'); ?>
                                              </div>
                             <div class="topShade"></div>
-							<div class="bottomShade"></div>			
-    
-    
-
-    
- 
-
-  <?php endwhile; ?>
-<?php endif; ?>
-<?php restore_current_blog(); ?>
-<?php wp_reset_query(); ?>
-											<?php //the_field('feature_container_3'); ?>
-                                           	
+							<div class="bottomShade"></div>	
 							
 							<div class="windows8">
 							<div class="wBall" id="wBall_1">
@@ -202,6 +168,10 @@
 					</div>
 		</div>
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+		
+		<div class="pageFeatureBelowBoxes">
+			<?php the_content(); ?>
+		</div>
 	</div><!-- .entry-content -->
 	<footer class="entry-meta">
 		
